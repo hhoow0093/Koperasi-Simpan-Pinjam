aa523dc237be52c9a80d7730cffb38f6f31be668
hhoow0093 <howarddai2005@gmail.com>
2025-12-18 22:22:03 +0700

    generate apk

--- app/src/main/java/eu/tutorials/koperasi_simpan_pinjam/ui/theme/Theme.kt
+++ app/src/main/java/eu/tutorials/koperasi_simpan_pinjam/ui/theme/Theme.kt
@@ -10,14 +10,6 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
-
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80,
-    onSurface = white
-)
-
 private val LightColorScheme = lightColorScheme(
     primary = DeepBlue,
     secondary = PurpleGrey40,
@@ -36,7 +28,6 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun KoperasiSimpanPinjamTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
@@ -44,10 +35,9 @@ fun KoperasiSimpanPinjamTheme(
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+            dynamicLightColorScheme(context)
         }
 
-        darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
 
